cc88951d7255a4a0e44a7caf67a53159d159f374
mohdbinharun56 <arahaman461@gmail.com>
2024-07-28 12:44:22 +0600

    a songs array has been created and two methods: create, findAll in songService

--- src/songs/songs.service.ts
+++ src/songs/songs.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from "@nestjs/common";
 
 @Injectable()
-export class SongsService{}
+export class SongsService{
+    // local DB
+    // local array
+    private readonly songs= []; 
+
+    create(song){
+        // save the song in the DB
+        this.songs.push(song);
+        return this.songs;
+    }
+
+    findAll(){
+        // fetch the song from the DB
+        return this.songs;
+    }
+}
