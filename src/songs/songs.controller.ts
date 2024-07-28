80857789c49458ee2833cec1871bdf15b824ae20
mohdbinharun56 <arahaman461@gmail.com>
2024-07-28 12:16:13 +0600

    create the route endpoints in the songs.controller.ts

--- src/songs/songs.controller.ts
+++ src/songs/songs.controller.ts
@@ -1,6 +1,29 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Delete, Get, Post, Put } from "@nestjs/common";
 
 @Controller('songs')
 export class SongsController{
-    
+    @Post()
+    create(){
+        return "create a new song endoints";
+    }
+
+    @Get()
+    findAll(){
+        return "Find all the songs";
+    }
+
+    @Get(':id')
+    findOne(){
+        return "fetch song based on id";
+    }
+
+    @Put(':id')
+    update(){
+        return "update an existing song based on id";
+    }
+
+    @Delete(':id')
+    delete(){
+        return "Delete specific song based on the id";
+    }
 }
