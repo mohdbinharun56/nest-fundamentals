ebfdc1c1e816d056f3ff91a1592502c386f94ac0
mohdbinharun56 <arahaman461@gmail.com>
2024-07-31 11:00:26 +0600

    extracted the loggermiddleware classes too implement the nestmiddleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestMiddleware, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SongsModule } from './songs/songs.module';
+import { LoggerMiddleware } from './common/middleware/logger/logger.middleware';
 
 // Module serves the foundational building block of a nest.js application.
 @Module({
@@ -10,4 +11,8 @@ import { SongsModule } from './songs/songs.module';
   providers: [AppService],
   exports: [],
 })
-export class AppModule {}
+export class AppModule implements NestModule{
+  configure(consumer: MiddlewareConsumer){
+    consumer.apply(LoggerMiddleware).forRoutes('songs');
+  }
+}
